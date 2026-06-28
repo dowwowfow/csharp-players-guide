a911d1e5c52a8990c415d9be7c7ea606f9303f57
Jesse Griswold <jgriswold816@gmail.com>
2026-06-27 21:04:03 -0700

    add Level 12-P2: LawsOfFreach (foreach loops, type casting, int.MaxValue)

---
+++ LevelTwelveProjectTwo/LawsOfFreach/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = new int[] {4, 51, -7, 13, -99, 15, -8, 45, 90};
+
+int total = 0;
+int currentSmallest = int.MaxValue;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number < currentSmallest)
+    {
+        currentSmallest = number;
+    }
+}
+
+float average = (float)total / numbers.Length;
+
+Console.WriteLine($"The average of the array is {average}, and the smallest number is {currentSmallest}");
