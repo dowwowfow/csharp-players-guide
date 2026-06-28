c9b5315f852a3b943cb989be7933d699417362d4
Jesse Griswold <jgriswold816@gmail.com>
2026-06-27 21:03:58 -0700

    add Level 11-P2: MagicCanon (for loops, modulo, console colors)

---
+++ Level11Project2/MagicCanon/Program.cs
@@ -0,0 +1,27 @@
+﻿
+for(int shot = 1; shot <= 20; shot++)
+{
+    if (shot % 3 == 0 && shot % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{shot}: Electric and Fire");
+        
+    }
+    else if( shot % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{shot}:Electric");
+    }
+    else if(shot % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{shot}:Fire");
+    }
+    else
+    {
+        Console.ForegroundColor= ConsoleColor.White;
+        Console.WriteLine($"{shot}: Normal");
+    }
+}
+
+Console.ResetColor();  
