b6b113174cced74087dd25a9f4ef1fe10c5b2e96
Jesse Griswold <jgriswold816@gmail.com>
2026-06-27 21:04:06 -0700

    add Level 13-P1: TakingANumber (local functions, parameters, return values)

---
+++ LevelThirteenProject1/TakingANumber/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int result = AskForANumber("Pick a number any number! ");
+Console.WriteLine(result);
+
+int range = AskForNumberInRange("Guess a number! ", 10, 20);
+Console.WriteLine($"Your guess of {range} was in the RANGE!");
+
+int AskForANumber(string text)
+{
+    int number = 0;
+    Console.Write(text);
+    number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = 0;
+        Console.Write(text);
+        number = int.Parse(Console.ReadLine());
+        if (number >= min && number <= max)
+        {
+            return number;
+        }
+        Console.WriteLine("Try Again!");
+    }
+}
