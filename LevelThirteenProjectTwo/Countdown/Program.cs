4b925a8c2dc318f2ec736ddfb374859b2b8e143a
Jesse Griswold <jgriswold816@gmail.com>
2026-06-27 21:04:08 -0700

    add Level 13-P2: Countdown (recursion, base case)

---
+++ LevelThirteenProjectTwo/Countdown/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Pick a number to count down from: ");
+int number = int.Parse(Console.ReadLine());
+Countdown(number);
+
+void Countdown(int number)
+{
+    if(number == 0)
+    {
+        Console.WriteLine("Finished Counting Down!");
+        return;
+    }
+    Console.WriteLine(number);
+    Countdown(number - 1);
+}
