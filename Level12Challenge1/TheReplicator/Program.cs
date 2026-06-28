b1fe7628c675bfb94df54d4c56cac55e007dc657
Jesse Griswold <jgriswold816@gmail.com>
2026-06-27 21:04:01 -0700

    add Level 12-C1: TheReplicator (arrays, indexing)

---
+++ Level12Challenge1/TheReplicator/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arrayOfNumbers = new int[5];
+
+for(int index = 0; index < arrayOfNumbers.Length; index++)
+{
+    Console.WriteLine("Add a number to the array: ");
+    int input = int.Parse(Console.ReadLine());
+    arrayOfNumbers[index] = input;
+}
+
+int[] anotherArray = new int[5];
+
+for (int index = 0;index < anotherArray.Length; index++)
+{
+    anotherArray[index] = arrayOfNumbers[index];
+}
+
+Console.WriteLine("Here is the original array you created: ");
+for (int index = 0; index < arrayOfNumbers.Length; index++)
+{
+    
+    Console.WriteLine(arrayOfNumbers[index]);
+}
+
+Console.WriteLine("Here is the contents of the new array: ");
+for (int index = 0; index < anotherArray.Length; index++)
+{
+    
+    Console.WriteLine(anotherArray[index]);
+}
