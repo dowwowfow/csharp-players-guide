da9de317b06e563627250f1da2443cb956c9b22c
Jesse <jgriswold816@gmail.com>
2026-07-01 14:53:30 -0700

    Add Level 16 - Simula's Test: chest state machine using enums and tuple switch

---
+++ Level16SimulasTest/SimulasTest/Program.cs
@@ -0,0 +1,36 @@
+﻿ChestState chestState = ChestState.Locked;
+string userInput;
+string question = "What do you want to do? ";
+do
+{
+    Console.Write($"The chest is {chestState}: ");
+    userInput = GetInput(question);
+
+    ChestState previousState = chestState;
+    chestState = (chestState, userInput) switch
+    {
+        (ChestState.Locked, "unlock") => ChestState.Closed,
+        (ChestState.Closed, "open") => ChestState.Opened,
+        (ChestState.Closed, "lock") => ChestState.Locked,
+        (ChestState.Opened, "close") => ChestState.Closed,
+        _ => chestState
+    };
+
+    if (chestState == previousState) 
+    {
+        Console.WriteLine($"The Chest is {chestState}, you cannot {userInput} it!");
+     }
+
+} while (true);
+
+
+string GetInput(string question)
+{
+    Console.Write(question);
+    string input = Console.ReadLine().ToLower();
+    return input;
+}
+
+
+
+enum ChestState {Opened, Closed, Locked};
